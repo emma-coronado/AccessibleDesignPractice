e09c1ce4bf19a1eb41b9f713998c97936b41969e
emma-coronado <ecoronado@ufl.edu>
2025-06-11 14:03:13 -0400

    add remaining sections and start on the accordion

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ window.onscroll = () => {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         navbar.classList.remove("dark");
         navbar.classList.add("light");
-        navbar.style.boxShadow = "0 6px 10px 2px rgba(0, 0, 0, 0.15)";
+        navbar.style.boxShadow = "0px 0px 12px 12px rgba(0, 0, 0, 0.05)";
+        navbar.style.borderBottom = "1px solid rgba(0, 38, 87, 0.30)";
     }
     else {
         navbar.classList.remove("light");
         navbar.classList.add("dark");
         navbar.style.boxShadow = "";
+        navbar.style.borderBottom = "";
     }
 };
 
