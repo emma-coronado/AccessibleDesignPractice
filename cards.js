2e88e206652730ce2ae450acc72c244f08da6bde
emma-coronado <ecoronado@ufl.edu>
2025-06-12 15:29:27 -0400

    finish card implementation and responsiveness and start on next section

--- cards.js
+++ cards.js
@@ -1,7 +1,24 @@
 async function addCards() {
     const temp = await fetch ('cardContents.json');
+    const cardContent = await temp.json();
     console.log(temp);
 
+    var cardHTML = "";
+
+    cardContent.cards.forEach(element => {
+        cardHTML += `<div class="card">
+                        <div class="img-placeholder dark">
+                            <p>Place Logo or Image for tool here</p>
+                        </div>
+                        <div class="card-body">
+                            <h3 class="h4">${element.title}</h3>
+                            <p>${element.description}</p>
+                            <a href="${element.link}">Link to learn more about tool →</a>
+                        </div>
+                    </div>`;
+    });
+
+    document.getElementById('tool-cards').insertAdjacentHTML("beforeend", cardHTML);
 }
 
 addCards();
